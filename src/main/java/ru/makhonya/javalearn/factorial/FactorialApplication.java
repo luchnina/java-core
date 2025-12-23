6186f81c71b02dfa28eed96a751b5cb68b282e91
luchnina <verminyMakr@mail.ru>
2025-12-23 12:57:29 +0300

    add error handling

--- src/main/java/ru/makhonya/javalearn/factorial/FactorialApplication.java
+++ src/main/java/ru/makhonya/javalearn/factorial/FactorialApplication.java
@@ -4,9 +4,20 @@ import java.math.BigInteger;
 
 public class FactorialApplication {
 
-    public static BigInteger calculationFactorial(int n) {
-        return (n == 1 || n == 0) ?
-                BigInteger.ONE :
-                BigInteger.valueOf(n).multiply(calculationFactorial(n - 1));
-    }
+	/**
+	 * Вычисляет факториал n рекурсивно.
+	 * @param n неотрицательное целое число
+	 * @return n! как BigInteger
+	 * @throws IllegalArgumentException если n < 0
+	 */
+	public static BigInteger calculationFactorial(int n) {
+
+		if (n < 0) {
+			throw new IllegalArgumentException("n должен быть неотрицательным, получился: " + n);
+		}
+
+		return (n == 1 || n == 0) ?
+				BigInteger.ONE :
+				BigInteger.valueOf(n).multiply(calculationFactorial(n - 1));
+	}
 }
