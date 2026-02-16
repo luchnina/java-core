6131bd3e41e8b6832788e1067ea939b75b8ed8e0
luchnina <verminyMakr@mail.ru>
2026-02-16 17:17:27 +0300

    add generic-based array organization

---
+++ src/main/java/ru/makhonya/javalearn/arraygeneric/ArrayApplication.java
@@ -0,0 +1,34 @@
+package ru.makhonya.javalearn.arraygeneric;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Arrays;
+
+public class ArrayApplication {
+
+  private final static Logger log = LoggerFactory.getLogger(ArrayApplication.class);
+
+  static void main() {
+    Array<Integer> array = Array.of(Integer.class);
+    Array<Integer> array1 = Array.of(Integer.class);
+
+    array.add(1);
+    array.add(2);
+    array.add(3);
+    array.add(4);
+    array.add(5);
+    array.add(6);
+
+    array1.add(123);
+    array1.add(345345);
+
+    array.addAll(array1);
+
+    array.copyOfRange(array, 1, 4);
+
+    log.info(Arrays.toString(array.toArray()));
+    log.info(Arrays.toString(array1.toArray()));
+  }
+
+}
